d64b2f4aba55e8a35e6943eae0f86f1fd4d90853
Shota Iwamoto <amaginqoo@gmail.com>
2018-07-19 19:58:38 -0700

    gulp構成修正20180719

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,30 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var plumber = require('gulp-plumber');
+var notify  = require('gulp-notify');
+var gcmq = require('gulp-group-css-media-queries');
+
 
 gulp.task('sass', function(){
-          return gulp.src('./sass/**/*.scss')
-            .pipe(sass({outputStyle: 'expanded'}))
-            .pipe(gulp.dest('./css'));
-          });
-
-gulp.task('sass:watch', function(){
-    gulp.watch('./sass/**/*.scss', ['sass']);
+  return gulp.src('./sass/*.scss')
+    .pipe(plumber({
+    errorHandler: notify.onError("Error: <%= error.message %>") //<-
+     }))
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(gcmq())
+    .pipe(gulp.dest('./'))
+    .pipe(connect.reload())
 });
+gulp.task('watch', function(){
+  gulp.watch('./sass/*.scss', ['sass']);
+  gulp.watch('./*.html', ['watch']);
+});
+
+gulp.task('serve', function(){
+  connect.server({
+  livereload: true
+  });
+});
+
+gulp.task('default', ['sass', 'watch', 'serve']);
